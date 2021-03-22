aa56526a005ddd54790770be11ef6b471dce99e7
Carolyn Saund <carolyn.saund@gmail.com>
2021-03-22 09:52:01 +0000

    fixed bug in cloud listing script

--- data/download/check_cloud_data.py
+++ data/download/check_cloud_data.py
@@ -34,16 +34,31 @@ args = parser.parse_args()
 AUDIO_BUCKET = 'audio_bucket_rock_1'
 TRANSCRIPT_BUCKET = 'audio_transcript_buckets_1'
 
-if __name__ == "__main__":
-    df = pd.read_csv(os.path.join(args.base_path, "videos_links.csv"))
-    df = df[df['speaker'] == args.speaker]
-    speaker_video_links = df['video_fn'].unique()
-    print("Got %s videos for speaker %s" % (str(len(speaker_video_links)), args.speaker))
-    possible_wavs = [v.replace('mp4', 'wav').replace('webm', 'wav').replace('mkv', 'wav') for v in speaker_video_links]
-    possible_transcripts = [w.replace('wav', 'csv') for w in possible_wavs]
+SPEAKERS = ['jon', 'ellen', 'oliver', 'almaram', 'angelica', 'seth', 'chemistry', 'rock', 'conan', 'shelly']
+
+
+def list_videos_for_speaker(df, speaker):
+    sdf = df[df['speaker'] == speaker]
+    speaker_video_links = sdf['video_fn'].unique()
+    print("Got %s videos for speaker %s" % (str(len(speaker_video_links)), speaker))
+    speaker_wavs = [v.replace('mp4', 'wav').replace('webm', 'wav').replace('mkv', 'wav') for v in speaker_video_links]
+    speaker_wavs_no_ext = [v.replace('mp4', '').replace('webm', '').replace('mkv', '') for v in speaker_video_links]
+    possible_transcripts = [w.replace('wav', 'csv') for w in speaker_wavs]
     current_wavs = list_blobs(AUDIO_BUCKET)
-    current_speaker_wavs = [w for w in possible_wavs if w in current_wavs]
+    current_speaker_wavs = [w for w in current_wavs if (w in speaker_wavs_no_ext or w in speaker_wavs)]
     current_transcripts = list_blobs(TRANSCRIPT_BUCKET)
     current_speaker_transcripts = [t for t in possible_transcripts if t in current_transcripts]
-    print('Found %s/%s wav files' % (str(len(current_speaker_wavs)), str(len(possible_wavs))))
+    print('Found %s/%s wav files' % (str(len(current_speaker_wavs)), str(len(speaker_wavs))))
     print('Found %s/%s csv files' % (str(len(current_speaker_transcripts)), str(len(possible_transcripts))))
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(os.path.join(args.base_path, "videos_links.csv"))
+    if args.speaker:
+        print('Cloud data available for', args.speaker, ':')
+        list_videos_for_speaker(df, args.speaker)
+    else:
+        for s in SPEAKERS:
+            print('Cloud data available for', s)
+            list_videos_for_speaker(df, s)
+
